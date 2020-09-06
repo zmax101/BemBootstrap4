06b230bcbc9154d96e053697a1c27760a1eb8413
zmax101 <69588727+zmax101@users.noreply.github.com>
2020-09-07 00:21:28 +0300

    Адаптивность

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    $('.button[filter]').click(function() {
-        if($(this).attr('filter')=='all') {
-            if($(this).attr('val')=='off') {
+$(document).ready(function () {
+    $('.button[filter]').click(function () {
+        if ($(this).attr('filter') == 'all') {
+            if ($(this).attr('val') == 'off') {
                 $('.button[filter]').attr('val', 'off');
                 $(this).attr('val', 'on');
                 $('.button[filter]').removeClass('focused');
@@ -9,23 +9,70 @@ $(document).ready(function() {
                 $('.filter > div').show(300);
             }
         } else
-        if($(this).attr('val')=='off') {
+        if ($(this).attr('val') == 'off') {
             $('.button[filter]').attr('val', 'off');
             $(this).attr('val', 'on');
             $('.button[filter]').removeClass('focused');
             $(this).addClass('focused');
             $('.filter > div').hide(300);
             let filter = $(this).attr('filter');
-            $('.filter > div[filter='+filter+']').show(300);
+            $('.filter > div[filter=' + filter + ']').show(300);
         }
     })
     // Слайдер
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: false,
-    dots: true,
-    dotsClass: 'dots-style'
-  });
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots-style',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
+    });
+    // Меню
+    $('ul.menu a[href^="#"').click(function () {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({'color':'#212121'});
+        $(this).css({'color':'#004bee'});
+        return false;
+    })
+    // выпадающее меню
+    $('.menu-icon').click(function(){
+        $('nav').slideToggle(500);
+        $('ul.menu').css({
+            'display':'flex', 'flex-direction':'column'
+        })
+        if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
+            $(this).html('<i class="fas fa-times"></i>')
+        } else {
+            $(this).html('<i class="fas fa-bars"></i>')
+        }
+    })
+    // кнопка вверх
+    $(window).scroll(function() {
+        if ($(this).scrollTop() != 0)
+        $('#toTop').fadeIn();
+        else
+        $('#toTop').fadeOut;
+    })
+    $('#toTop').click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+    })
 })
