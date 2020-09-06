bb0fcd030015d54692468935a61f1d8149420889
zmax101 <69588727+zmax101@users.noreply.github.com>
2020-09-06 19:13:49 +0300

    Раздел команды и слайдер

--- main.js
+++ main.js
@@ -19,4 +19,13 @@ $(document).ready(function() {
             $('.filter > div[filter='+filter+']').show(300);
         }
     })
+    // Слайдер
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+    dotsClass: 'dots-style'
+  });
 })
